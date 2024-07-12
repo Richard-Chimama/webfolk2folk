ba2eac17f4653a64b5e548f67e428de66d27ff61
richard <rich.chime@gmail.com>
2024-07-12 09:31:48 +0200

    change the port

--- folk2folk/vite.config.ts
+++ folk2folk/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv'
 import react from '@vitejs/plugin-react'
+
+dotenv.config()
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port: 3000
+  server: {
+    port: PORT
   }
 })
